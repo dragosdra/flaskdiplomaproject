54182cd4674b96002bf01e4b6c98984771b47620
dragosdra <dvoda2001@gmail.com>
2023-11-09 20:11:14 +0200

    Imported Flask-Login and SQLAlchemy

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from  flask import Flask
 from views import views
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,template_folder="templates", static_folder="static")
 app.register_blueprint(views, url_prefix="/")
