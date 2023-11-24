3cedca141006566c6978a0690eef17414c31a10b
dragosdra <dvoda2001@gmail.com>
2023-11-24 17:29:21 +0200

    modified darkroom page pt1,added JS

--- static/script.js
+++ static/script.js
@@ -1,3 +1,14 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+  $(document).ready(function() {
+ 
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+ 
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+ 
+});
