4d7c83a0df62355e5043d9ab2fb7cbf3445273a2
dragosdra <dvoda2001@gmail.com>
2024-01-05 00:13:08 +0200

    Added Contact Page,Changed overall page designs,integrated flask-login and sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from  flask import Flask
+from  flask import Flask, request
 from views import views
-from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__,template_folder="templates", static_folder="static")
 app.register_blueprint(views, url_prefix="/")
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True,port=2001)
+    app.run(debug=True,port=5000)
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request
 
 views = Blueprint(__name__,"views")
 
-
+#configuring routes for web app
+@views.route("/register")
 @views.route("/")
 def home():
     return render_template("index.html")
@@ -12,3 +13,18 @@ def products():
 @views.route("/darkroom")
 def darkroom():
     return render_template("darkroom.html")
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
+@views.route('/register' ,methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        #handle request
+        pass
+    return render_template(register.html)
+@views.route('/login' ,methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        #handle request
+        pass
+    return render_template(login.html)
